435e479274f6fc8b118588e47ae5c9707d3604b2
KirllB <kirillbv@mail.ru>
2019-07-09 11:49:45 +0700

    redesign

--- script.js
+++ script.js
@@ -55,5 +55,21 @@ $(document).ready(function() {
     ]
      });
    
-
+     
+    
 });
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
